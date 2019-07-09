535e8c266baccc9ad5a087a71e80941f3a14a415
nashaofu <diaocheng@outlook.com>
2019-07-09 22:53:54 +0800

    add  添加cors中间件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import Koa from 'koa'
-import http from 'http'
-import path from 'path'
-import https from 'https'
 import fs from 'fs-extra'
+import cors from '@koa/cors'
 import parser from 'koa-parser'
 import Router from 'koa-router'
 import request from './request'
@@ -11,23 +9,20 @@ const app = new Koa()
 const router = new Router()
 
 router.post('/image', async (ctx, next) => {
+  if (!ctx.request.body) return ctx.throw(400, '请求参数非法')
+  if (!ctx.request.body.file) return ctx.throw(400, '请求参数非法')
   const file = await fs.readFile(ctx.request.body.file.path)
   const { data } = await request(file)
   ctx.body = JSON.stringify(data)
   await next()
 })
-const options = {
-  key: fs.readFileSync(path.join(__dirname, '../ssl/server.key')), // ssl文件路径
-  cert: fs.readFileSync(path.join(__dirname, '../ssl/server.pem')) // ssl文件路径
-}
+
+app.use(cors())
 app.use(parser())
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-http.createServer(app.callback()).listen(3001)
-https.createServer(options, app.callback()).listen(3000)
-
-// app.listen(3000, (err) => {
-//   if (err) throw err
-//   console.log('app listen port: 3000')
-// })
+app.listen(3000, err => {
+  if (err) throw err
+  console.log('http port: 3000')
+})
